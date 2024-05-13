8679e03fb024407bbed4f39a76c0f19e2ce7a6d3
rigo0809 <169068411+rigo0809@users.noreply.github.com>
2024-05-13 23:36:39 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+      public static void main(String[] args) {
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println("********************");
+        System.out.println("*Filme : Crepústulo *");
+        System.out.println("********************");
+        System.out.println(" ");
+        System.out.println("Lançamento do Filme Crepústulo");
+        System.out.println("19 de dezembro de 2008 ");
+        System.out.println(" ");
+        System.out.println("Sinopse ");
+
+        System.out.println("======================================================");
+
+        System.out.println("A estudante Bella Swan conhece Edward Cullen, um belo mas misterioso adolescente. Edward é um vampiro, cuja família não bebe sangue, e Bella, longe de ficar assustada, se envolve em um romance perigoso com sua alma gêmea imortal.");
+        System.out.println("====================================================== ");
+        System.out.println(" ");
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("+ Endereço do Trailer +");
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("  ");
+
+        System.out.println("https://www.youtube.com/watch?v=21I6vX7zKdE");
+        System.out.println(" ");
+        System.out.println("Opinião: Perfeição ");
+        System.out.println(" ");
+
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+  }
+}
